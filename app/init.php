b8df27d4caed1719866af026a7e73a5851ae6fcb
Rodrigo Lucena <rodrigo@rslucena.com.br>
2021-02-25 19:08:58 -0300

    Reconstruction of the way that sessions are created and manipulated

--- app/init.php
+++ app/init.php
@@ -2,13 +2,15 @@
 
     declare(strict_types = 1);
 
+    use app\interfaces\AuthInterface;
+
     #DEFINES
     require_once 'configs/Settings.php';
 
     require_once sprintf("%s/autoload.php", DIR_VENDOR);
 
     #START SESSION
-    session_start();
+    AuthInterface::build();
 
     #INIT APP
     require_once sprintf("%s/Run.php", DIR_BOOT);
